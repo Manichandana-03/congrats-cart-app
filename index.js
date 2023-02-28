7ec8b906bd9ce7197b0e0a0343be74f078372ad5
Manichandana-03 <manivasam03@gmail.com>
2023-02-28 07:33:56 +0000

    congrats

---
+++ index.js
@@ -0,0 +1,15 @@
+const element = (
+  // Write your code here.
+
+  <div className="bg-container">
+    <h1 className="heading"> Congraculations</h1>
+    <div className="cards">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h3>Kiran V</h3>
+      <p>JNTUH University College of Engineering Manthani</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
